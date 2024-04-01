be50558a0bdd6a99bf078b830175f25017d83d7f
Ashok <ashok@MacBook.local>
2024-04-02 09:48:45 +1100

    iam roles update

--- infra/iam.tf
+++ infra/iam.tf
@@ -4,25 +4,38 @@ resource "google_service_account" "service_account" {
   display_name = "Cloud Function Service Account"
 }
 
-resource "google_project_iam_custom_role" "cfunction_role" {
-  role_id     = "customCFunctionRole"
-  title       = "Custom Cloud Function Role"
-  description = "Permissions for Cloud Function to interact with GCS and BigQuery"
-  permissions = [
-    "storage.objects.get",
-    "eventarc.events.receiveEvent",
-    "eventarc.events.receiveAuditLogWritten",
-    "run.executions.cancel",
-    "run.jobs.run",
-    "run.routes.invoke"
-  ]
-}
 
-resource "google_project_iam_binding" "service_account_binding" {
+resource "google_project_iam_member" "member-role" {
+  for_each = toset([
+    "roles/eventarc.eventReceiver",
+    "roles/run.invoker",
+    "roles/pubsub.publisher",
+    "roles/storage.objectUser",
+  ])
+  role    = each.key
+  member  = "serviceAccount:${google_service_account.service_account.email}"
   project = var.project_id
-  role    = google_project_iam_custom_role.cfunction_role.id
-
-  members = [
-    "serviceAccount:${google_service_account.service_account.email}",
-  ]
 }
+
+# resource "google_project_iam_custom_role" "cfunction_role" {
+#   role_id     = "customCFunctionRole"
+#   title       = "Custom Cloud Function Role"
+#   description = "Permissions for Cloud Function to interact with GCS and BigQuery"
+#   permissions = [
+#     "storage.objects.get",
+#     "eventarc.events.receiveEvent",
+#     "eventarc.events.receiveAuditLogWritten",
+#     "run.executions.cancel",
+#     "run.jobs.run",
+#     "run.routes.invoke"
+#   ]
+# }
+
+# resource "google_project_iam_binding" "service_account_binding" {
+#   project = var.project_id
+#   role    = google_project_iam_custom_role.cfunction_role.id
+
+#   members = [
+#     "serviceAccount:${google_service_account.service_account.email}",
+#   ]
+# }
